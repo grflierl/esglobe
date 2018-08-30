9a85641fadfc53d58679dce22e00fdd784303ad8
glenn <glenn@lake.mit.edu>
2018-08-30 17:22:02 -0400

    esg, ht

--- ht.php
+++ ht.php
@@ -1,7 +1,12 @@
 <?php
-header("Content-type: image/jpg");
+$url=substr($_SERVER["PATH_INFO"],1);
+if(strrpos($url,".mp4")>0){
+  header("Content-type: video/mp4");
+} else {
+  header("Content-type: image/jpg");
+};
 $qs=$_SERVER["QUERY_STRING"];
-$cmd="curl ".substr($_SERVER["PATH_INFO"],1);
+$cmd="curl ".$url;
 if(strlen($qs)>0){
   $cmd=$cmd."?".$qs;
 };
