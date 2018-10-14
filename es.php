531b3bcfa6c7684c7336b7d44fa53fea9f8d3be6
glenn <glenn@lake.mit.edu>
2018-10-14 09:20:59 -0400

    es with icons

--- es.php
+++ es.php
@@ -159,18 +159,6 @@ function init(){
   var sph = window.sph;
 //  sph.baseurl="/esglobe/307/graphics/"
 </script>
-<!--
-<img src="cmd.jpg"
-     style='position:absolute;left:1000px;top:10px;width:48px;height:216px'
-     usemap='#ctlmap'/>
-<map name='ctlmap'>
-  <area shape='rect' coords='0,0,47,35' href='javascript:sph.orient(0,0)'>
-  <area shape='rect' coords='0,36,47,71' href='javascript:sph.rot(0.005)'>
-  <area shape='rect' coords='0,72,47,110' href='javascript:sph.rot(-0.005)'>
-  <area shape='rect' coords='0,111,47,152' href='javascript:sph.pause()'>
-  <area shape='rect' coords='0,153,47,215' href='javascript:sph.stop()'>
-</map>
--->
 <img src="cmd2.jpg"
      style='position:absolute;left:1000px;top:10px;width:48px;height:208px'
      usemap='#ctlmap'/>
@@ -190,13 +178,6 @@ function init(){
 <span id='menu'></span>
 </div>
 </li>
-<!--
-<li><a href='javascript:sph.orient(0,0)'>Recenter</a></li>
-<li><a href='javascript:sph.rot(0.005)'><img src='ccw2.png'/></a></li>
-<li><a href='javascript:sph.rot(-0.005)'><img src='cw2.png'/></a></li>
-<li><a href='javascript:sph.pause()'>Pause/resume</a></li>
-<li><a href='javascript:sph.stop()'>Stop movie</a></li>
--->
  </ul>
 <iframe id='ifr' name="pages" src="http:<?php echo $url?>" height=900px width=800px>
 </iframe>
